d4fd89588d6e04d99a038331a96303859acd16a7
Remmy Clay <remmy.lauren@gmail.com>
2017-11-17 09:48:14 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -13,8 +13,8 @@ end
 
 def retrieval
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-
   #your code here!
+  shipping_manifest[oil paintings]
 end
 
 def adding
