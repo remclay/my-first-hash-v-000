25611166bed48bd558aa5bb0fee95b1f6ebc956c
Remmy Clay <remmy.lauren@gmail.com>
2017-11-17 09:42:46 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,8 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
   my_hash = {"dog" => "Monty", "fish" => "Goldie", "bird" => "Blitz"}
-
-
 end
 
 
@@ -10,7 +8,10 @@ def shipping_manifest
   #set a variable called `shipping_manifest`, equal to a hash
   #fill that hash with key/value pairs that describe the following information:
   #We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
-
+  shipping_manifest = {}
+  shipping_manifest[whale bone corsets] = 5
+  shipping_manifest[porcelain vases] = 2
+  shipping_manifest[oil paintings] = 3
 
 end
 
